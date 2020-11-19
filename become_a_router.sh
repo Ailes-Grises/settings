451129e1cc45ef58415d6bbd982626482279dd9f
Ailes-Grises <toriniku.n.s@gmail.com>
2020-11-20 01:46:48 +0900

    become_a_router.sh を追加

---
+++ become_a_router.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+# このスクリプトは，Wifiが使えないPC1(SOTEC のCentOSとか)とまともなPC2(Panasonic のUbuntuとか)を有線接続することによって，
+# PC1をネットに接続できるようにするものである．
+
+# 1. CentOS(Wifiが使えない)上で，NIC の設定を行い，ゲートウェイとDNSをUbuntuのIPアドレスにしておく．
+
+# 2. ルーティングテーブルを作成する(Ubuntuにとってのルータをゲートウェイとして設定する)
+sudo route add default gw 192.168.10.1
+
+# 3. Wifiに直接繋ぐPC上で，ファイアウォールの設定を変更する(IPマスカレードの設定とフィルタの設定)
+sudo iptables -A FORWARD -i enp0s25 -o wlp3s0 -j ACCEPT
+sudo iptables -A FORWARD -i wlp3s0 -o enp0s25 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -o wlp3s0 -j MASQUERADE
+
+# フォワーディングの設定
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo sysctl -p
