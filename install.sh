3a1e87b8e600fd19a3a02f7702c412ba2416c357
Ailes-Grises <toriniku.n.s@gmail.com>
2021-07-11 20:32:07 +0900

    modify: install.sh にpicocom とinput_utils を追加した

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ sudo apt-get -y install python3-pip
 
 # Basic tools
 sudo apt-get -y install zsh upower curl gparted source-highlight imagemagick
-sudo apt-get -y install git tig mycli bat
+sudo apt-get -y install git tig mycli bat pococom input_utils
 
 # Languages
 sudo apt-get -y install gcc g++ gdb libc6-dev-i386
