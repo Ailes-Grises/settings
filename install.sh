a717adf5edd02807fc6e4082b666b25d5cb31f37
Ailes-Grises <toriniku.n.s@gmail.com>
2021-07-13 01:27:08 +0900

    modify: install.sh を編集

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ sudo apt-get -y install python3-pip
 # Basic tools
 sudo apt-get -y install zsh upower curl gparted source-highlight imagemagick
 sudo apt-get -y install git tig mycli bat pococom input_utils
-sudo apt-get -y install manpages-ja manpages-ja-dev sleepenv
+sudo apt-get -y install manpages-ja manpages-ja-dev sleepenh
 
 # Languages
 sudo apt-get -y install gcc g++ gdb libc6-dev-i386
